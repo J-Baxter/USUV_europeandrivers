90983679fd605d2c6badcd8adb8f8845aab04472
James Baxter <james.baxter@ed.ac.uk>
2023-12-08 18:09:26 +0000

    public genbank data figures

--- scripts/figures.R
+++ scripts/figures.R
@@ -25,79 +25,99 @@ my_theme <- theme_classic(base_family = "LM Sans 10")+
 
 data <- read_csv('./data/genbank_usuv_20231206.csv') %>%
   select(-1) %>% 
-  filter(seq.length >= 300) %>%
-  filter(collection.date.formatted > '1980-01-01') %>%
-  group_by(host.order) %>%
-  filter(n() >= 5) %>%
-  ungroup() 
-  
+  filter(seq.length >= 300) 
 
-data %>%
-  group_by(month = floor_date(collection.date.formatted, "season"), iso.country) %>%
+
+plt1 <-data %>%
+  group_by(year= floor_date(collection.date.formatted, "year"), iso.country, is.europe) %>%
   summarise(n = n()) %>%
-  ggplot() +
-  geom_line(aes(x = month, y = n, colour = iso.country))
+  ggplot() + 
+  geom_tile(aes(x = year, y = iso.country, fill=n)) + 
+  scale_x_date('Collection Year') + 
+  scale_y_discrete('Country') + 
+  scale_fill_distiller(palette = 'OrRd', direction = 1) + 
+  my_theme + 
+  facet_grid(rows = vars(is.europe), scales = 'free_y', space = 'free_y',switch="both") + 
+  theme(legend.position = 'bottom', strip.placement = 'outside')
+
+ggsave(plot = plt1, filename = 'plt1.jpeg', device = jpeg,  width = 170, height = 120,  units = 'mm', dpi = 350)
 
 
-
-plt2 <- data %>%  
-  filter(seq.length >= 300) %>%
-  filter(collection.date.formatted > '1990-01-01') %>%
-  group_by(host.order) %>%
-  filter(n() >= 5) %>%
-  ungroup() %>%
-  ggplot() +
-  geom_histogram(aes(x = collection.date.formatted, fill = host.order)) +
-  scale_fill_brewer(palette = 'BuGn', na.value = 'black') + 
-  facet_wrap(.~iso.country, scales = 'free_y') + 
+plt2 <- data %>%
+  ggplot()+
+  #geom_rect(aes(xmin = 9000, xmax = Inf, ymin = 0, ymax = Inf), alpha = 0.007, fill = 'lightgrey')+
+  geom_histogram(aes(x = seq.length, fill = segment), binwidth = 250) +
+  geom_vline(xintercept  = 9000, linetype = 'dashed') + 
+  geom_vline(xintercept  = 300, linetype = 'dashed') + 
+  annotate('text', label = 'NFLG', x= 9500, y = 350, size = 3) +
+  scale_x_continuous('Sequence Length', expand = c(0,0), breaks = seq(0,10000, by= 1000)) + 
+  scale_y_continuous('Count', expand = c(0,0)) +
+  scale_fill_brewer(palette = 'PuBu', na.value = 'black', 'Genomic Region') + 
   my_theme + 
   theme(legend.position = 'bottom')
+  
+ggsave(plot = plt2, filename = 'plt2.jpeg', device = jpeg,  width = 170, height = 120,  units = 'mm', dpi = 350)
 
-ggsave(plot = plt2, filename = 'plt2.jpeg', device = jpeg,  width = 190, height = 150,  units = 'mm', dpi = 350)
+
+
+
+
+  #group_by(month = floor_date(collection.date.formatted, "season"), iso.country) %>%
+ # summarise(n = n()) %>%
+  #ggplot() +
+  #geom_line(aes(x = month, y = n, colour = iso.country))
+
+
+
+#plt2 <- data %>%  
+  #filter(seq.length >= 300) %>%#
+  #filter(collection.date.formatted > '1990-01-01') %>%
+  #group_by(host.order) %>%
+  #filter(n() >= 5) %>%
+  #ungroup() %>%
+ # ggplot() +
+  #geom_histogram(aes(x = collection.date.formatted, fill = host.order)) +
+  #scale_fill_brewer(palette = 'BuGn', na.value = 'black') + 
+  #facet_wrap(.~iso.country, scales = 'free_y') + 
+  #my_theme + 
+  #theme(legend.position = 'bottom')
+
+#ggsave(plot = plt2, filename = 'plt2.jpeg', device = jpeg,  width = 190, height = 150,  units = 'mm', dpi = 350)
+
 
 plt3 <- data %>%  
   filter(seq.length >= 300) %>%
-  filter(collection.date.formatted > '1990-01-01') %>%
   filter(host.class == 'Aves') %>%
   group_by(host.order) %>%
-  filter(n() > 5) %>%
-  group_by(host.genus) %>%
   summarise(n = n()) %>%
   mutate(host.freq = n / sum(n)) %>%
   ungroup() %>%
-  left_join(data %>% select(c(host.genus, host.order)) %>% distinct()) %>%
   ggplot() +
-  geom_bar(aes(x = host.order, y = host.freq, fill = host.genus), stat = 'identity') +
-  #scale_fill_brewer(palette = 'Paired', na.value = 'black') + 
-  #facet_wrap(.~iso.country, scales = 'free_y') + 
-  my_theme + 
-  theme(legend.position = 'right')
+  geom_bar(aes(x = host.order, y = host.freq, fill = '#fdbb84'), stat = 'identity') +
+  scale_x_discrete('Host Order') + 
+  scale_y_continuous(expand = c(0,0), 'Relative Frequency', limits =c(0,1.05), breaks = seq(0,1, by = 0.2))+
+  my_theme 
 
 ggsave(plot = plt3, filename = 'plt3.jpeg', device = jpeg,  width = 150, height = 150,  units = 'mm', dpi = 350)
 
 plt4 <- data %>%  
-  filter(seq.length >= 300) %>%
-  filter(collection.date.formatted > '1990-01-01') %>%
   filter(host.order == 'Diptera') %>%
-  group_by(host.genus) %>%
-  filter(n() > 5) %>%
-  group_by(host.species) %>%
+  group_by(host.genus, host.species) %>%
+  #filter(n() > 5) %>%
+  #group_by(host.species) %>%
   summarise(n = n()) %>%
+  ungroup(host.genus) %>%
   mutate(host.freq = n / sum(n)) %>%
   ungroup() %>%
-  inner_join(data %>% 
-             filter(host.order == 'Diptera') %>%
-               group_by(host.genus) %>%
-               filter(n() > 5) %>% 
-               ungroup() %>% 
-               select(c(host.species, host.genus)) %>%
-               distinct() %>% 
-               filter(if_any(everything(), ~ !is.na(.))))  %>%
   ggplot() +
-  geom_bar(aes(x = host.genus, y = host.freq, fill = host.species), stat = 'identity') +
-  scale_fill_brewer(palette = 'BuGn', na.value = 'black') + 
-  #facet_wrap(.~iso.country, scales = 'free_y') + 
+  geom_bar(aes(x = host.species, y = host.freq, fill = '#fdbb84'), stat = 'identity') +
+  scale_x_discrete('Mosquito Species') + 
+  scale_y_continuous(expand = c(0,0), 'Relative Frequency', limits =c(0,1.05), breaks = seq(0,1, by = 0.2))+
+  #scale_fill_brewer(palette = 'Paired') + 
+  facet_grid(cols = vars(host.genus), scales = 'free_x', space = 'free_x', switch="both") +
   my_theme + 
-  theme(legend.position = 'right')
+  theme( strip.placement = 'outside') 
 
-ggsave(plot = plt4, filename = 'plt4.jpeg', device = jpeg,  width = 150, height = 150,  units = 'mm', dpi = 350)
+
+
+ggsave(plot = cowplot::plot_grid(plt3, plt4, align = 'hv', labels = c('Bird Order', 'Vector Species'), label_size = 10, label_fontfamily = "LM Sans 10", nrow = 2, hjust = -0.9), filename = 'plt4.jpeg', device = jpeg,  width = 190, height = 220,  units = 'mm', dpi = 350)
