8614bc782ef91a505baae350ae56e6d9d2d043e9
James Baxter <39841697+J-Baxter@users.noreply.github.com>
2024-08-24 15:16:59 +0100

    draft subsampling for region. issue with israel data identified.

--- scripts/subsampling_global.R
+++ scripts/subsampling_global.R
@@ -52,9 +52,9 @@ ExtractTerminalBranchLengths <- function(tree, aln_length, snp_threshold = 0){
     group_by(parent) %>%
     mutate(n = n()) %>% 
     filter(n > 1) %>%
-    mutate(group = cur_group_id()) %>%
+    mutate(sequence_group = cur_group_id()) %>%
     ungroup() %>%
-    select(c(label, group)) %>%
+    select(c(label, sequence_group)) %>%
     rename(tipnames = label)
     
     
@@ -72,9 +72,67 @@ groups <- mapply(ExtractTerminalBranchLengths,
 data_per_alignment_wgroups <- data_per_alignment %>%
   left_join(groups)
 
+subsample_1 <- data_per_alignment_wgroups %>%
+  group_by(alignment) %>%
+  
+  # infer best location code and create grouping factor
+  mutate(best_location_code = coalesce(collection_subdiv1code,
+                                       collection_countrycode)) %>%
+  mutate(sequence_group = as.factor(sequence_group)) %>%
+  
+  # group by sequence_identity > best_location > host_class
+  
+  group_by(alignment,
+           sequence_group,
+           best_location_code,
+           host_class) %>%
+  
+  slice(which(date_ym == max(date_ym) | date_ym == min(date_ym))) %>%
+  distinct() %>%
+  ungroup() %>%
+  group_split(alignment) %>%
+  setNames(aln_names)
+
+#nflg -> 126 sequences
+#ns5_9100-9600 -> 302 sequences
+# NB some identical sequences preserved (min and max within same location code)
+
 
 #################### Subsampling 2 #################### 
 # Details subsampling with respect to region. Here, the ML tree is traversed such that each time 
 # region changes, the two most distant (temporally) tips are selected until the region next changes.
 # This is applied across the entire 'global' dataset.
 
+test <- as_tibble(t) %>%
+  rename(tipnames = label) %>%
+  left_join(data_per_alignment_wgroups %>% 
+              filter(alignment == 'nflg') %>% 
+              select(c(tipnames, collection_regionname))) %>%
+  filter(grepl('\\|', tipnames)) %>%
+  mutate(collection_regionchange = cumsum(collection_regionname != lag(collection_regionname, def = first(collection_regionname)))) %>% 
+  group_by(Tag, LocationChange, location)
+  
+
+
+subsample_1 <- data_per_alignment_wgroups %>%
+  #(left join tidytree with tr)
+  
+  group_by(alignment) %>%
+  
+  # infer best location code and create grouping factor
+  mutate(best_location_code = coalesce(collection_subdiv1code,
+                                       collection_countrycode)) %>%
+  mutate(sequence_group = as.factor(sequence_group)) %>%
+  
+  # group by sequence_identity > best_location > host_class
+  
+  group_by(alignment,
+           sequence_group,
+           best_location_code,
+           host_class) %>%
+  
+  slice(which(date_ym == max(date_ym) | date_ym == min(date_ym))) %>%
+  distinct() %>%
+  ungroup() %>%
+  group_split(alignment) %>%
+  setNames(aln_names)
