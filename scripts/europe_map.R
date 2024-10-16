364e3f8043bdf38d4357511e92423cfd64404016
James Baxter <james.baxter@ed.ac.uk>
2024-10-16 19:01:51 +0100

    link metadata to new nuts2 geounits

--- scripts/europe_map.R
+++ scripts/europe_map.R
@@ -31,6 +31,12 @@ lu_spatialdata <- readRDS('./spatial_data/Predictors_tep_20231014.Rdata')
 test_df <- lu_spatialdata %>% 
   dplyr::select(c(lon, lat, Deciduous_broad))
 
+# metadat
+metadata %>%
+  drop_na(c('collection_subdiv1lat', 'collection_subdiv1long')) %>%
+  st_as_sf(coords = c('collection_subdiv1lat', 'collection_subdiv1long'), crs = 4326) %>%
+  st_join(., nuts2)
+
 
 ggplot(nuts2) + 
   geom_sf() + 
@@ -39,17 +45,17 @@ ggplot(nuts2) +
 
 
 coordinates(test_df) <- ~ lon + lat
-r <- raster(extent(test_df), resolution = 0.1)
+r <- raster(extent(test_df), resolution = 0.008333333)
 crs(r) <- CRS("+proj=longlat +datum=WGS84")
 
 raster_test <- rasterize(test_df, r, field = 'Deciduous_broad', fun = mean)
 
-library(raster)
+chicken <- raster('~/Downloads/Chickens_Mekong.asc/Chickens_Mekong.asc')
+plot(chicken)
 
-raster::extract()
+chicken <- raster('~/Downloads/GLW4-2020.D-DA.CHK.tif')
+plot(chicken)
 
+cattle <- raster('~/Downloads/GLW4-2020.D-DA.CTL.tif')
+plot(log10(cattle))
 
-ggplot(data = country) +
-  geom_sf(aes()) +
-  geom_point(data = test, mapping = aes(x = lon, y = lat, color = tmp))+
-  theme_bw()
