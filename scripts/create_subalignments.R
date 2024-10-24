2204fe924e9651dc8793a20f660cc6630fdf4cda
James Baxter <james.baxter@ed.ac.uk>
2024-10-24 20:10:29 +0100

    move initial subalignment code from data formatting script

--- scripts/create_subalignments.R
+++ scripts/create_subalignments.R
@@ -34,3 +34,43 @@ mapply(write.dna,
        sub_alignments,
        sub_alignment_names,
        format = 'fasta')
+
+
+
+###
+
+#############  Sub-aligments ############# 
+write.dna(alignment[rownames(alignment) %in% (metadata_noFLI %>% 
+                                                filter(generegion_nflg == 1) %>% 
+                                                pull(tipnames)),],
+          './2024Oct20/alignments/USUV_2024Oct20_alldata_aligned_formatted_noFLI_NFLG.fasta', 
+          format = 'fasta')
+
+
+# Alignment has 953 sequences with 665 columns, 385 distinct patterns, 152 parsimony-informative, 80 singleton sites, 433 constant sites
+write.dna(alignment[rownames(alignment) %in% (metadata_noFLI %>% 
+                                                filter(generegion_NS5_9000_9600 == 1) %>% 
+                                                pull(tipnames)),],
+          './2024Oct20/alignments/USUV_2024Oct20_alldata_aligned_formatted_noFLI_NS5_9000-9600.fasta', 
+          format = 'fasta')
+
+# 
+#write.dna(alignment[rownames(alignment) %in% (metadata_noFLI %>% 
+#  filter(generegion_NS5_9100_9600 == 1) %>%
+# pull(tipnames)),],
+#'./2024Oct20/alignments/USUV_2024Oct20_alldata_aligned_formatted_noFLI_NS5_9100-9600.fasta', 
+# format = 'fasta')
+
+
+write.dna(alignment[rownames(alignment) %in% (metadata_noFLI %>% 
+                                                filter(generegion_NS5_10042_10312 == 1) %>% 
+                                                pull(tipnames)),],
+          './2024Oct20/alignments/USUV_2024Oct20_alldata_aligned_formatted_noFLI_NS5_10042-10312.fasta', 
+          format = 'fasta')
+
+
+write.dna(alignment[rownames(alignment) %in% (metadata_noFLI %>%
+                                                filter(generegion_env_1003_1491 == 1) %>%
+                                                pull(tipnames)),],
+          './2024Oct20/alignments/USUV_2024Oct20_alldata_aligned_formatted_noFLI_env_1003-1491.fasta', 
+          format = 'fasta')
