c5f05aa22f908b5dec37250511045aee08ee1984
James Baxter <james.baxter@ed.ac.uk>
2024-10-31 19:15:54 +0000

    refactoring to improve readability

--- scripts/seraphim.R
+++ scripts/seraphim.R
@@ -5,15 +5,17 @@ library(diagram)
 
 # seraphim plots
 
-allTrees <- scan(file = './2024Oct20/alignments/subset_alignments/run/USUV_2024Oct20_NFLG_A_subsampled_traits_1000.trees',
+allTrees <- scan(file = './2024Oct20/alignments/subset_alignments/run/USUV_2024Oct20_NFLG_B_subsampled_traits_1000.trees',
                  what = '',
                  sep = '\n',
                  quiet = T)
-localTreesDirectory = "./2024Oct20/alignments/subset_alignments/A/"
+TREEFILE <- './2024Oct20/alignments/subset_alignments/run/USUV_2024Oct20_NFLG_B_subsampled_traits_mcc.tree'
+
+localTreesDirectory = "./2024Oct20/alignments/subset_alignments/B/"
 burnIn <- 0
 randomSampling <- FALSE
-nberOfTreesToSample <- 1000
-mostRecentSamplingDatum <- decimal_date(c_most_recent_date %>% ym())
+nberOfTreesToSample <- 500
+mostRecentSamplingDatum <- decimal_date(b_most_recent_date %>% ymd())
 coordinateAttributeName <- "location"
 treeExtractions(localTreesDirectory,
                 allTrees,
@@ -21,38 +23,54 @@ treeExtractions(localTreesDirectory,
                 randomSampling, 
                 nberOfTreesToSample, 
                 mostRecentSamplingDatum,
-                coordinateAttributeName)
+                coordinateAttributeName,
+                nberOfCores = 16)
 
-
-mcc_tree <-readAnnotatedNexus('./2024Oct20/alignments/subset_alignments/run/USUV_2024Oct20_NFLG_A_subsampled_traits_mcc.tree')
+mcc_tree <-read.beast(TREEFILE)
 mcc_tree_tbl <- as_tibble(mcc_tree)
 source("./imported_scripts/mccExtractions.r") # Script obtained from the GitHub tutorial folder.
 
-mcc_tab <- mccExtractions(mcc_tre, mostRecentSamplingDatum)
+mcc_tab <- mccExtractions(readAnnotatedNexus(TREEFILE), mostRecentSamplingDatum)
 
 # Step 4: Estimating the HPD region for each time slice ----
 nberOfExtractionFiles <- nberOfTreesToSample
 prob <- 0.95
 precision <- 0.1 # time interval that will be used to define the successive time slices
 startDatum <- min(mcc_tab[,"startYear"])
-polygons <- suppressWarnings(spreadGraphic2(localTreesDirectory, nberOfExtractionFiles, prob, startDatum, precision))
 
-# Step 6: Co-plotting the HPD regions and MCC tree ----
-# Step 6: Co-plotting the HPD regions and MCC tree ----
+# Format Polygons
+polygons <- suppressWarnings(spreadGraphic2(localTreesDirectory, 
+                                            nberOfExtractionFiles,
+                                            prob, 
+                                            startDatum, 
+                                            precision))
+
+sf_list <- lapply(polygons, st_as_sf) 
+
+sf_combined <- bind_rows(sf_list) %>%
+  pivot_longer(cols = starts_with('2'), names_to = 'year', values_to = 'value') %>%
+  filter(value == 1) %>%
+  mutate(year = as.numeric(year)) %>%
+  dplyr::select(-value) %>%
+  st_set_crs(4326) 
+
+# Set Base Map
 map <-all_europe_sf
-# plot nodes on map
-nodes <- mcc_tre_tbl %>%
+
+
+# Format Nodes
+nodes <- mcc_tree_tbl %>%
   dplyr::select(node,height, location1, location2) %>%
-  mutate(year = decimal_date(ym(c_most_recent_date)) - as.numeric(height)) %>%
+  mutate(year = decimal_date(ymd(b_most_recent_date)) - as.numeric(height)) %>%
   st_as_sf(coords = c( 'location2', 'location1'), 
            crs = 4326)
 
-
-arrows <-  mcc_tre_tbl %>%
+# Format Arrows
+arrows <-  mcc_tree_tbl %>%
   dplyr::select(node, parent) %>%
-  left_join(mcc_tre_tbl %>%
+  left_join(mcc_tree_tbl %>%
               dplyr::select(node, location1, location2)) %>%
-  left_join(mcc_tre_tbl %>%
+  left_join(mcc_tree_tbl %>%
               dplyr::select(node, location1, location2),
             by = join_by(parent== node)) %>%
   mutate(across(starts_with('location'), .fns = ~ as.numeric(.x))) %>%
@@ -61,15 +79,7 @@ arrows <-  mcc_tre_tbl %>%
          location2.y = case_when(location2.y == location2.x ~ location2.y + 0.00000001,
                                  .default = location2.y))
 
-sf_list <- lapply(polygons, st_as_sf) 
-sf_combined <- bind_rows(sf_list) %>%
-  pivot_longer(cols = starts_with('2'), names_to = 'year', values_to = 'value') %>%
-  filter(value == 1) %>%
-  mutate(year = as.numeric(year)) %>%
-  dplyr::select(-value) %>%
-  st_set_crs(4326) 
-
-
+# Plot in GGplot
 ggplot(map) +
   geom_sf() +
   
@@ -93,7 +103,7 @@ ggplot(map) +
                        option = 'C')+
   
   
-  coord_sf(ylim = c(40,60), xlim = c(-5, 20), expand = FALSE) +
+  coord_sf(ylim = c(40,60), xlim = c(-2, 33), expand = FALSE) +
   theme_void(base_size = 18) + 
   guides(fill = guide_colourbar(
                                 theme = theme(
