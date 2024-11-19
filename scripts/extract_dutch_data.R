e2c97f8999f78fcbfe54e366706209d9f8a0313b
James Baxter <james.baxter@ed.ac.uk>
2024-11-19 15:13:04 +0000

    reformat erasmus data

--- scripts/extract_dutch_data.R
+++ scripts/extract_dutch_data.R
@@ -27,9 +27,12 @@ original_format <- read_csv('./erasmus_data/240422_LiveDeadWildCapBirds_NL_16_22
 
 ############################################## MAIN ################################################
 
-# alignment with unique IDs
+# Only non-published sequences
+original_format %<>%
+  filter(is.na(GenBank))
+
 erasmus_labels <- original_format %>%
-  unite(., label, ID, DeathOrSampleDate, sep = '|') %>%
+  unite(., label, UniqueID, DeathOrSampleDate, sep = '|') %>%
   pull(label)
 
 erasmus_sequences <- original_format %>%
@@ -39,12 +42,28 @@ erasmus_sequences <- original_format %>%
   as.DNAbin()
 
 erasmus_data <- original_format %>% 
-  dplyr::select(-Sequence)
+  dplyr::select(-c(Sequence,
+                   1,
+                   ID,
+                   DateInfo,
+                   English,
+                   family,
+                   order,
+                   Remark,
+                   Surveillance,
+                   Long,
+                   Lat,
+                   CT)) %>%
+  unite(., 'Geo_Location', postal.code, location, province, sep = ' ') %>%
+  rename(Isolate = UniqueID,
+         Collection_Date = DeathOrSampleDate,
+         Host = Latin,
+         Isolation_Source = Sample) 
 
 ############################################## WRITE ###############################################
 
 write_csv(erasmus_data,
-          './erasmus_data/erasmus_data.csv')
+          './data/erasmus_data.csv')
 
 write.FASTA(erasmus_sequences,
             './erasmus_data/erasmus_sequences.fasta')
