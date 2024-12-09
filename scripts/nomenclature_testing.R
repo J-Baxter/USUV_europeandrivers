8a1e55c93f9ec504cebef1fd8b1ce587a1450c30
James Baxter <james.baxter@ed.ac.uk>
2024-12-09 18:06:59 +0000

    plot updated with 'old' lineages

--- scripts/nomenclature_testing.R
+++ scripts/nomenclature_testing.R
@@ -22,6 +22,8 @@ library(ggtree)
 library(ggtreeExtra)
 library(ggsci)
 library(ggnewscale)
+library(colorspace)
+library(scales)
 
 # User functions
 GroupSequences <- function(aln, snp_threshold = 0){
@@ -96,6 +98,14 @@ metadata_in_tree <- read_csv('./data/USUV_metadata_all_2024Dec02.csv') %>%
 
 stopifnot(nrow(metadata_in_tree) == Ntip(ml_tree)) #sanity check
 
+
+# import lineages and tree (nextstrain) from autolin
+autolin_labels <- read_tsv('./2024Dec02/nomenclature/labels.tsv') %>%
+  rename(label = sample)
+
+test <- treeio::read.nextstrain.json('./2024Dec02/nomenclature/annotated.json')
+
+
 ############################################## MAIN ################################################
 most_recent_date <- metadata_in_tree %>%
   pull(date_ymd) %>% #note explicit assumption that most recent date will be ymd not ym - NB this no longer holds for hungary
@@ -150,114 +160,20 @@ lineage_nodes <- subtrees %>%
   
 
   
-autolin_labels <- read_tsv('./2024Dec02/nomenclature/labels.tsv') %>%
-  rename(label = sample) %>%
-  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2', 'level_3'),
-                       too_few = 'align_start') %>%
-  mutate(level_3 = case_when(!is.na(level_3) ~ paste(level_0, level_1, level_2, level_3, sep = '.')),
-         level_2 = case_when(!is.na(level_2) ~ paste(level_0, level_1, level_2, sep = '.')),
-         level_1 = case_when(!is.na(level_1) ~ paste(level_0, level_1, sep = '.')))
+# %>%
+#  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2', 'level_3'),
+#                       too_few = 'align_start') %>%
+#  mutate(level_3 = case_when(!is.na(level_3) ~ paste(level_0, level_1, level_2, level_3, sep = '.')),
+#         level_2 = case_when(!is.na(level_2) ~ paste(level_0, level_1, level_2, sep = '.')),
+#         level_1 = case_when(!is.na(level_1) ~ paste(level_0, level_1, sep = '.')))
 
 
-midpoint.root(ml_tree) %>% 
-
-  #left_join(metadata_in_tree %>% 
-             # dplyr::select(is_europe, sequence_accession, nuts0_id, tipnames, lineage, host_class) %>%
-            #  rename(label = tipnames),
-          #  by = 'label') %>%
-  
- # left_join(tree_tbl) %>% 
- # mutate(wellsupported = ifelse( UFboot > 0.7, '1', '0')) %>%
-  
-  left_join(autolin_labels) %>%
-  
- # left_join(lineage_nodes) %>%
-  ggtree() + 
-  theme_tree2() + 
-  
-  geom_cladelabel()
-
-  geom_fruit(geom = geom_tile,
-             mapping = aes(fill = level_0),
-             width = 0.002,
-             colour = NA,
-             pwidth = 1.2,
-             offset = 0.03) + 
-  geom_fruit(geom = geom_text,
-             mapping = aes(label = level_0),
-             offset = 0.03) + 
-  geom_fruit(geom = geom_tile,
-             mapping = aes(fill = level_1),
-             width = 0.002,
-             colour = "white",
-             pwidth = 1.2,
-             offset = 0.06)
-  scale_fill_brewer('Location', palette = 'Dark2', direction = -1, 
-                    labels = c('0' = 'Outside of Europe',
-                               '1' = 'Within Europe'),
-                    guide = guide_legend(keywidth = 1.5, keyheight = 1, ncol = 1, order = 2)) 
-  #geom_text(aes(label=node), hjust=-.3)
-  #scale_colour_distiller(palette = 'OrRd', direction = 1)
-  
-
-
+# determine ancestral nodes of each lineage level
 primary_lineages <- autolin_labels %>%
   select(lineage) %>%
   filter(!grepl('\\.', lineage)) %>%
   distinct() %>%
   rename(level_0 = lineage)
-  
-level_0_cols <- pal_d3(palette = "category10")(nrow(primary_lineages))
-
-primary_lineages %<>%
-  cbind.data.frame(primary_lineage_cols)
-
-level_1_cols <- autolin_labels %>%
-  select(lineage) %>%
-  distinct() %>%
-  #filter(grepl('\\.', lineage)) %>%
-  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2'),
-                       too_few = 'align_start') %>%
-  select(-level_2) %>%
-  distinct() %>%
-  mutate(level_1  = as.integer(level_1)) %>%
-  group_by(level_0) %>%
-  arrange(level_1,.by_group = TRUE) %>%
-  filter(!(is.na(level_1) & n() > 2)) %>%
-  mutate(n = cur_group_id()) %>%
-  mutate(level_1_col = lighten(primary_lineage_cols[n], 
-                               amount = seq(0.2, 0.8, length.out = n()))) %>%
-  ungroup() %>%
-  drop_na() %>%
-  pull(level_1_col)
-
-
-level_2_cols <- autolin_labels %>%
-  select(lineage) %>%
-  distinct() %>%
-  #filter(grepl('\\.', lineage)) %>%
-  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2'),
-                       too_few = 'align_start') %>%
-  select(-level_1) %>%
-  distinct() %>%
-  mutate(level_2  = as.integer(level_2)) %>%
-  group_by(level_0) %>%
-  arrange(level_2,.by_group = TRUE) %>%
-  filter(!(is.na(level_2) & n() > 2)) %>%
-  mutate(n = cur_group_id()) %>%
-  mutate(level_2_col = lighten(primary_lineage_cols[n], 
-                               amount = seq(0.2, 0.8, length.out = n()))) %>%
-  ungroup() %>%
-  drop_na() %>%
-  pull(level_2_col)
-
-separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2', 'level_3'),
-                     too_few = 'align_start') %>%
-  mutate(level_3 = case_when(!is.na(level_3) ~ paste(level_0, level_1, level_2, level_3, sep = '.')),
-         level_2 = case_when(!is.na(level_2) ~ paste(level_0, level_1, level_2, sep = '.')),
-         level_1 = case_when(!is.na(level_1) ~ paste(level_0, level_1, sep = '.')))
-
-test <- treeio::read.nextstrain.json('./2024Dec02/nomenclature/annotated.json')
 
 test_mrca_level0 <- test@data %>%
   filter(`GRI Lineage Level 0` != 'not assigned') %>%
@@ -277,7 +193,6 @@ test_mrca_level1 <- test@data %>%
   drop_na()
 
 
-
 test_mrca_level2 <- test@data %>%
   filter(`GRI Lineage Level 2` != 'not assigned') %>%
   group_by(`GRI Lineage Level 2`) %>%
@@ -287,7 +202,61 @@ test_mrca_level2 <- test@data %>%
   mutate(label = case_when(!grepl('\\.[[:digit:]]\\.', label) ~ NA_character_, .default = label)) %>%
   drop_na()
 
+old_nomenclature <- as_tibble(cbind.data.frame(
+  label = c('EU1', 'EU2', 'EU3', 'EU4', 'EU5', 'AF1', 'AF2', 'AF3'),
+  node = c(688, 691, 621, 619, 604, 485, 480, 496))) %>%
+  drop_na()
 
+
+
+# set colour scheme
+level_0_cols <- pal_d3(palette = "category10")(nrow(primary_lineages))
+
+primary_lineages %<>%
+  cbind.data.frame(level_0_cols)
+
+level_1_cols <- autolin_labels %>%
+  select(lineage) %>%
+  distinct() %>%
+  #filter(grepl('\\.', lineage)) %>%
+  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2'),
+                       too_few = 'align_start') %>%
+  select(-level_2) %>%
+  distinct() %>%
+  mutate(level_1  = as.integer(level_1)) %>%
+  group_by(level_0) %>%
+  arrange(level_1,.by_group = TRUE) %>%
+  filter(!(is.na(level_1) & n() > 2)) %>%
+  mutate(n = cur_group_id()) %>%
+  mutate(level_1_col = lighten(level_0_cols[n], 
+                               amount = seq(0.2, 0.8, length.out = n()))) %>%
+  ungroup() %>%
+  drop_na() %>%
+  pull(level_1_col)
+
+
+level_2_cols <- autolin_labels %>%
+  select(lineage) %>%
+  distinct() %>%
+  #filter(grepl('\\.', lineage)) %>%
+  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2'),
+                       too_few = 'align_start') %>%
+  select(-level_1) %>%
+  distinct() %>%
+  mutate(level_2  = as.integer(level_2)) %>%
+  group_by(level_0) %>%
+  arrange(level_2,.by_group = TRUE) %>%
+  filter(!(is.na(level_2) & n() > 2)) %>%
+  mutate(n = cur_group_id()) %>%
+  mutate(level_2_col = lighten(level_0_cols[n], 
+                               amount = seq(0.2, 0.8, length.out = n()))) %>%
+  ungroup() %>%
+  drop_na() %>%
+  pull(level_2_col)
+
+
+
+# plot NFLG nextstrain (not time scaled) phylogeny
  p1 <- test %>% 
   ggtree() + 
   geom_cladelab(data = test_mrca_level0 , 
@@ -305,7 +274,7 @@ test_mrca_level2 <- test@data %>%
                  barsize = 0,
                  offset = 0.004,
                  offset.text = -0.0004) + 
-   scale_colour_manual(values = primary_lineage_cols) + 
+   scale_colour_manual(values = level_0_cols) + 
    new_scale_colour()+
    
    geom_cladelab(data = test_mrca_level1 , 
@@ -333,13 +302,37 @@ test_mrca_level2 <- test@data %>%
                  textcolour = 'black',
                  barsize = 0) + 
    
-   theme(legend.position = 'none') + geom_tiplab(align = TRUE, aes(label = ""))
+   theme(legend.position = 'none') +
+   new_scale_colour() +
+   geom_tiplab(align = TRUE, aes(label = "", colour =`GRI Lineage Level 0` )) +
+   scale_colour_manual(values = c(level_0_cols, 'white')) + 
+   
+   # old
+   new_scale_colour() +
+   geom_cladelab(data = old_nomenclature, 
+                 mapping = aes(node = node, label = label, colour = label, alpha = 0.05), 
+                 align = TRUE, 
+                 fontsize = 4, 
+                 textcolour = NA,
+                 barsize = 15,
+                 offset = 0.006) + 
+   scale_colour_brewer(palette = 'Pastel2')+
+   
+   geom_cladelab(data = old_nomenclature , 
+                 mapping = aes(node = node, label = label, colour = label), 
+                 align = TRUE, 
+                 fontsize = 4, 
+                 offset = 0.006,
+                 textcolour = 'black',
+                 offset.text = -0.0006,
+                 barsize = 0) 
+   
 
-
-p <- purrr::reduce(
+ # collapse level 2 lineages
+p <-purrr::reduce(
   test_mrca_level2$node,
   \(x,y) 
-    collapse(x,y,mode = "max",fill="black",size=0.05),
+  collapse(x, y, mode = "max", fill = "black", size = 0.05),
   .init = p1
 )
    
