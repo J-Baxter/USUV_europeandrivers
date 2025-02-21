5e6e6c2b4d1a4c5e0db8c8a47403aa7efc02402f
James Baxter <james.baxter@ed.ac.uk>
2025-02-21 18:47:56 +0000

    push nomenclature plots to git

---
+++ scripts/nomenclature_plots_dec24.R
@@ -0,0 +1,175 @@
+test <- treeio::read.nextstrain.json('./2024Dec02/nomenclature/annotated_05.json')
+autolin_labels <- read_tsv('./2024Dec02/nomenclature/labels_05.tsv') %>%
+  rename(label = sample)
+
+
+primary_lineages <- autolin_labels %>%
+  dplyr::select(lineage) %>%
+  filter(!grepl('\\.', lineage)) %>%
+  distinct() %>%
+  rename(level_0 = lineage)
+
+test_mrca_level0 <- test@data %>%
+  filter(`GRI Lineage Level 0` != 'not assigned') %>%
+  group_by(`GRI Lineage Level 0`) %>%
+  slice_min(num_date) %>% 
+  dplyr::select(node, country, starts_with('GRI')) %>%
+  rename(label = `GRI Lineage Level 0`)
+
+
+test_mrca_level1 <- test@data %>%
+  filter(`GRI Lineage Level 1` != 'not assigned') %>%
+  group_by(`GRI Lineage Level 1`) %>%
+  slice_min(num_date) %>% 
+  dplyr::select(node, country, starts_with('GRI')) %>%
+  rename(label = `GRI Lineage Level 1`) %>%
+  mutate(label = case_when(!grepl('\\.', label) ~ NA_character_, .default = label)) %>%
+  drop_na()
+
+
+test_mrca_level2 <- test@data %>%
+  filter(`GRI Lineage Level 2` != 'not assigned') %>%
+  group_by(`GRI Lineage Level 2`) %>%
+  slice_min(num_date) %>% 
+  dplyr::select(node, country, starts_with('GRI')) %>%
+  rename(label = `GRI Lineage Level 2`) %>%
+  mutate(label = case_when(!grepl('\\.[[:digit:]]\\.', label) ~ NA_character_, .default = label)) %>%
+  drop_na()
+
+old_nomenclature <- as_tibble(cbind.data.frame(
+  label = c('EU1', 'EU2', 'EU3', 'EU4', 'EU5', 'AF1', 'AF2', 'AF3'),
+  node = c(688, 691, 621, 619, 604, 485, 480, 496))) %>%
+  drop_na()
+
+
+
+# set colour scheme
+level_0_cols <- pal_d3(palette = "category10")(nrow(primary_lineages))
+
+primary_lineages %<>%
+  cbind.data.frame(level_0_cols)
+
+level_1_cols <- autolin_labels %>%
+  dplyr::select(lineage) %>%
+  distinct() %>%
+  #filter(grepl('\\.', lineage)) %>%
+  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2'),
+                       too_few = 'align_start') %>%
+  dplyr::select(-level_2) %>%
+  distinct() %>%
+  mutate(level_1  = as.integer(level_1)) %>%
+  group_by(level_0) %>%
+  arrange(level_1,.by_group = TRUE) %>%
+  filter(!(is.na(level_1) & n() > 2)) %>%
+  mutate(n = cur_group_id()) %>%
+  mutate(level_1_col = lighten(level_0_cols[n], 
+                               amount = seq(0.2, 0.8, length.out = n()))) %>%
+  ungroup() %>%
+  drop_na() %>%
+  pull(level_1_col)
+
+
+level_2_cols <- autolin_labels %>%
+  dplyr::select(lineage) %>%
+  distinct() %>%
+  #filter(grepl('\\.', lineage)) %>%
+  separate_wider_delim(lineage, delim = '.', names = c('level_0', 'level_1', 'level_2'),
+                       too_few = 'align_start') %>%
+  dplyr::select(-level_1) %>%
+  distinct() %>%
+  mutate(level_2  = as.integer(level_2)) %>%
+  group_by(level_0) %>%
+  arrange(level_2,.by_group = TRUE) %>%
+  filter(!(is.na(level_2) & n() > 2)) %>%
+  mutate(n = cur_group_id()) %>%
+  mutate(level_2_col = lighten(level_0_cols[n], 
+                               amount = seq(0.2, 0.8, length.out = n()))) %>%
+  ungroup() %>%
+  drop_na() %>%
+  pull(level_2_col)
+
+
+
+# plot NFLG nextstrain (not time scaled) phylogeny
+p1 <- test %>% 
+  ggtree() + 
+  geom_cladelab(data = test_mrca_level0 , 
+                mapping = aes(node = node, label = label, colour = label), 
+                align = TRUE, 
+                textcolour = NA,
+                barsize = 15,
+                offset = 0.004) + 
+  
+  geom_cladelab(data = test_mrca_level0 , 
+                mapping = aes(node = node, label = label, colour = label), 
+                align = TRUE, 
+                fontsize = 4, 
+                textcolour = 'black',
+                barsize = 0,
+                offset = 0.004,
+                offset.text = -0.0004) + 
+  scale_colour_manual(values = level_0_cols) + 
+  new_scale_colour()+
+  
+  geom_cladelab(data = test_mrca_level1 , 
+                mapping = aes(node = node, label = label, colour = label), 
+                align = TRUE, 
+                textcolour = NA,
+                barsize = 15,
+                offset = 0.0025) + 
+  
+  geom_cladelab(data = test_mrca_level1 , 
+                mapping = aes(node = node, label = label, colour = label), 
+                align = TRUE, 
+                fontsize = 4, 
+                textcolour = 'black',
+                barsize = 0,
+                offset = 0.0025,
+                offset.text = -0.00055) + 
+  scale_colour_manual(values = level_1_cols) + 
+  
+  new_scale_colour() +
+  geom_cladelab(data = test_mrca_level2 , 
+                mapping = aes(node = node, label = label, colour = label), 
+                align = TRUE, 
+                fontsize = 4, 
+                textcolour = 'black',
+                barsize = 0) + 
+  
+  theme(legend.position = 'none') +
+  new_scale_colour() +
+  geom_tiplab(align = TRUE, aes(label = "", colour =`GRI Lineage Level 0` )) +
+  scale_colour_manual(values = c(level_0_cols, 'white')) + 
+  
+  # old
+  new_scale_colour() +
+  geom_cladelab(data = old_nomenclature, 
+                mapping = aes(node = node, label = label, colour = label, alpha = 0.05), 
+                align = TRUE, 
+                fontsize = 4, 
+                textcolour = NA,
+                barsize = 15,
+                offset = 0.006) + 
+  scale_colour_brewer(palette = 'Pastel2')+
+  
+  geom_cladelab(data = old_nomenclature , 
+                mapping = aes(node = node, label = label, colour = label), 
+                align = TRUE, 
+                fontsize = 4, 
+                offset = 0.006,
+                textcolour = 'black',
+                offset.text = -0.0006,
+                barsize = 0) 
+
+
+# collapse level 2 lineages
+p <-purrr::reduce(
+  test_mrca_level2$node,
+  \(x,y) 
+  collapse(x, y, mode = "max", fill = "black", size = 0.05),
+  .init = p1
+)
+
+
+ggsave(p, filename = "~/Downloads/nomenclature_05.png", width = 13, height = 25)
+
