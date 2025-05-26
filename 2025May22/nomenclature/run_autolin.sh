05420e6cc278a53dd5a33c920daa39442134953e
James Baxter <james.baxter@ed.ac.uk>
2025-05-26 19:03:11 +0100

    init cli for autolin

---
+++ 2025May22/nomenclature/run_autolin.sh
@@ -0,0 +1,1 @@
+python annotate_json.py --input ./input_json/usuv.json --output usuv_lineages.json --size 2 --distinction 10 --cutoff 0.99 --levels 3 --floor 1 --labels labels.tsv --report report.tsv
