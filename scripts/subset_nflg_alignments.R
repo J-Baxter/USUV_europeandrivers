c3373e2bedb4f54008964b62f9aeaedfaf25d112
James <james.baxter@ed.ac.uk>
2025-07-04 14:09:21 +0800

    Update header, take june 2025 clusterings, updated sanity check to preserve continuity

--- scripts/subset_nflg_alignments.R
+++ scripts/subset_nflg_alignments.R
@@ -1,19 +1,19 @@
-####################################################################################################
-####################################################################################################
-## Script name:
-##
-## Purpose of script: Split the NFLG alignment according to a csv of lineage/cluster designations+
-##
-## Date created: 2024-10-29
-##
-##
-########################################## SYSTEM OPTIONS ##########################################
-options(scipen = 6, digits = 4) 
-memory.limit(30000000) 
+################################################################################
+## Script Name:        Split the NFLG alignment according to a csv of lineage/cluster designations+
+## Purpose:            <BRIEFLY_DESCRIBE_SCRIPT_PURPOSE>
+## Author:             James Baxter
+## Date Created:       2025-07-04
+################################################################################
 
-  
-########################################## DEPENDENCIES ############################################
-# Packages
+############################### SYSTEM OPTIONS #################################
+options(
+  scipen = 6,     # Avoid scientific notation
+  digits = 7      # Set precision for numerical display
+)
+memory.limit(30000000)
+
+############################### DEPENDENCIES ###################################
+# Load required libraries
 library(tidyverse)
 library(magrittr)
 library(tidyverse)
@@ -23,42 +23,45 @@ library(treeio)
 library(TreeTools)
 library(ggtree)
 
-# User functions
-# 
 
-############################################## DATA ################################################
-nflg_alignment <- read.dna('./2024Oct20/alignments/USUV_2024Oct20_alldata_aligned_formatted_noFLI_NFLG.fasta',
+################################### DATA #######################################
+# Read and inspect data
+nflg_alignment <- read.dna('./2025Jun24/alignments/USUV_2025Jun24_alldata_aligned_formatted_noFLI_NFLG.fasta',
                            as.matrix = T,
                            format = 'fasta')
 
-metadata <- read_csv('./data/USUV_metadata_all_2024Oct20.csv')
+metadata <- read_csv('./data/USUV_metadata_all_2025Jun24.csv')
 
-clusters <- read_csv('./data/clusters_2024Oct29.csv')
+clusters <- read_csv('./2025Jun24/europe_clusters/all_nflg_cluster.csv')
 
 metadata %<>%
-  left_join(clusters)
+  left_join(clusters,
+            by = join_by('tipnames' == 'label'))
 
-############################################## MAIN ################################################
 
-split_metadata <- metadata %>%
-  
+################################### MAIN #######################################
+# Main analysis or transformation steps
+split_metadata_by_cluster <- metadata %>%
   # all europe NFLG allocated a cluster - exclude the rest for now
   drop_na(cluster) %>%
-  
   group_split(cluster)
 
-split_alignments <- lapply(split_metadata, function(x) nflg_alignment[rownames(nflg_alignment) %in% x$tipnames,])
+# Sanity check - each sequence included only once (and all sequences are present)
+stopifnot(lapply(split_metadata_by_cluster, nrow) %>% unlist() %>% sum() == 
+            nrow( metadata %>% drop_na(cluster)))
 
-############################################## WRITE ###############################################
-filenames <- paste0('./2024Oct20/alignments/USUV_2024Oct20_alldata_aligned_formatted_noFLI_NFLG_',
-                   c('A', 'B', 'C', 'D', 'E'),
-                   '.fasta')
+split_alignments <- lapply(split_metadata_by_cluster, function(x) 
+  nflg_alignment[rownames(nflg_alignment) %in% x$tipnames,])
+
+################################### OUTPUT #####################################
+# Save output files, plots, or results
+filenames <- paste0('./2025Jun24/alignments/USUV_2025Jun24_alldata_aligned_formatted_noFLI_NFLG_',
+                    LETTERS[1:length(split_alignments)],
+                    '.fasta')
 
 mapply(write.dna,
        split_alignments,
        filenames,
        format = 'fasta')
-
-############################################## END #################################################
-####################################################################################################
-####################################################################################################
+#################################### END #######################################
+################################################################################
