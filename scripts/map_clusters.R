07be15b65301bc79fd60e0e019f2b78e181cde69
James <james.baxter@ed.ac.uk>
2025-07-04 13:52:46 +0800

    map clusters to all nflg sequences; visual inspection of similarity between lineage and clustering

--- scripts/map_clusters.R
+++ scripts/map_clusters.R
@@ -1,19 +1,21 @@
-####################################################################################################
-####################################################################################################
-## Script name: Map clusters to all NFLG sequences
-##
-## Purpose of script:
-##
-## Date created: 2024-10-29
-##
-##
-########################################## SYSTEM OPTIONS ##########################################
-options(scipen = 6, digits = 4) 
-memory.limit(30000000) 
+################################################################################
+## Script Name:        Map clusters and lineages to all NFLG data
+## Purpose:            <BRIEFLY_DESCRIBE_SCRIPT_PURPOSE>
+## Author:             James Baxter
+## Date Created:       2025-07-04
+################################################################################
 
-  
-########################################## DEPENDENCIES ############################################
-# Packages
+############################### SYSTEM OPTIONS #################################
+options(
+  scipen = 6,     # Avoid scientific notation
+  digits = 7      # Set precision for numerical display
+)
+memory.limit(30000000)
+
+############################### DEPENDENCIES ###################################
+# Load required libraries
+library(tidyverse)
+library(magrittr)
 library(tidyverse)
 library(magrittr)
 library(treeio)
@@ -23,47 +25,124 @@ library(ggnewscale)
 library(ggtreeExtra)
 library(phangorn)
 
+################################### DATA #######################################
+# Read and inspect data
+nflg_hipstr <- read.beast('./2025Jun24/global_analysis/USUV_2025Jun24_NFLG_SRD06_HMC_constant_hipstr.tree')
 
-# User functions
-
-
-############################################## DATA ################################################
-#nflg_traits <- read.beast('./2024Dec02/global_beast/USUV_2024Dec02_nflg_subsample1_SRD06_relaxLn_constant_mcc.tree')
-nflg_mcc <- read.beast('./2024Dec02/global_beast/USUV_2024Dec02_nflg_subsample1_SRD06_relaxLn_constant_mcc.tree')
-
-nflg_ml <- read.newick('./2024Dec02/alignments/USUV_2024Dec02_alldata_aligned_formatted_noFLI_NFLG.fasta.treefile') %>% 
+nflg_ml <- read.newick('./2025Jun24/global_analysis/phylogenetic_placement/USUV_2025Jun24_alldata_aligned_formatted_noFLI_NFLG_subsampled.fasta.contree') %>% 
   phytools::midpoint_root()
 
+subsample_clusterings <- read_csv('./2025Jun24/europe_clusters/subsample_clusterings.csv')
 
-metadata_in_beast_tree <- read_csv('./data/USUV_metadata_all_2024Dec02.csv') %>%
-  filter(tipnames %in% nflg_mcc@phylo$tip.label) 
+subsample_nomenclature <- read_delim('./2025Jun24/nomenclature/subsample/USUV_2025Jun24_labels.tsv') %>%
+  rename(label = sample)
 
-stopifnot(nrow(metadata_in_beast_tree ) == Ntip(nflg_mcc@phylo)) #sanity check
+################################### MAIN #######################################
+# Main analysis or transformation steps
+mrca_nodes <- nflg_ml %>%
 
-############################################## MAIN ################################################
-ml_tbl <- as_tibble(nflg_ml)
+  # ML tree to tree-data object and add cluster/lineage assignments
+  as_tibble() %>%
+  left_join(subsample_clusterings %>% dplyr::select(label, dist_30)) %>%
+  
+  # Exclude problematic cluster I
+  filter(dist_30 != 'I') %>%
+
+  # Exclude all NAs and split by cluster
+  drop_na(dist_30) %>%
+  group_split(dist_30) %>%
+  setNames(LETTERS[1:length(.)]) %>%
+  
+  # Find common ancestor nodes of clusters/lineages 
+  lapply(., function(x) MRCA(nflg_ml, .node1 = x$label)) %>%
+  enframe(name = 'cluster', value = 'root') %>%
+  mutate(root = as.integer(root))
 
 
-root_nodes <- c('A' = 595,
-                'B' = 751,
-                'C' = 839,
-                'D' = 578,
-                'E' = 581)
+# visual checks
+#nflg_ml %>%
+  #left_join(mrca_nodes %>% rename(node = root)) %>%
+  #ggtree() + 
+  #geom_nodepoint(aes(colour = cluster))
 
 
-# clusters inferred based on dist 50 threshold
-clusters <- lapply(root_nodes, function(x) ml_tbl %>% 
-                     filter(node %in% offspring(nflg_ml, x)) %>%
+# Find all descendants of common ancestor nodes
+all_seq_clusters <- mrca_nodes %>%
+  pull(root) %>%
+  lapply(. , function(x) ml_tbl %>% 
+                     filter(node %in% offspring(nflg_ml, x, type = 'tips')) %>%
                      pull(label)) %>%
   lapply(., as_tibble) %>%
+  setNames(LETTERS[1:length(.)]) %>%
   bind_rows(, .id = 'cluster') %>%
-  rename(tipnames = value)
+  rename(label = value)
 
-############################################## WRITE ###############################################
-write_csv(clusters,
-      './data/clusters_2024Oct29.csv')
-############################################## END #################################################
-####################################################################################################
+# visual checks
+nflg_ml %>%
+  left_join(all_seq_clusters) %>% 
+  ggtree(.) + 
+  geom_tippoint(aes(colour = cluster))
+  
 
+# Cluster X lineage comparison
+subsample_clusterings %>% dplyr::select(label, dist_30) %>%
+  left_join(subsample_nomenclature) %>%
+  dplyr::select(-1) %>%
+  distinct() %>%
+  view()
 
+# visualise (ugly)
+lineage_colours <- c('A' = '#a4243b',
+                     'B' = '#be776b',
+                     'C' = '#d8c99b',
+                     'D' = '#d8b06c', 
+                     # 'E' =  '#d8973c', 
+                     'E' = '#cb7d36', 
+                     'F' = '#bd632f', 
+                     'G' = '#273e47')
 
+nflg_hipstr %>%
+  left_join(all_seq_clusters) %>% 
+  ggtree(mrsd = '2024-10-12') +
+
+geom_cladelab(data = level_0_tbl %>% rename(lineage_label = lineage) %>% 
+                mutate(level_0 = gsub('\\..*', '', lineage_label)),
+              mapping = aes(node = root_node, 
+                            label = lineage_label, 
+                            colour = level_0),
+              offset = -32,
+              offset.text = -17,
+              fontsize = 3, 
+              #textcolour = NA,
+              #size = 0.01,
+              align = T) + 
+  scale_colour_manual(values = lineage_colours, guide = 'none')+
+  new_scale_colour() +
+  
+  # Label level 1
+  geom_cladelab(data = level_1_tbl %>% rename(lineage_label = lineage) %>% 
+                  mutate(level_0 = gsub('\\..*', '', lineage_label)),
+                mapping = aes(node = root_node, 
+                              label = lineage_label, 
+                              colour = level_0),
+                offset = -20,
+                offset.text = -17,
+                # size = 0.01,
+                align = TRUE, 
+                fontsize = 3, 
+                #textcolour = NA,
+                #barsize = 15,
+  ) + 
+  scale_colour_manual(values = lineage_colours, guide = 'none') +
+  new_scale_colour() + 
+  geom_tippoint(aes(colour = cluster))
+  
+
+################################### OUTPUT #####################################
+# Save output files, plots, or results
+
+# all nflg clusters
+write_csv(all_seq_clusters, './2025Jun24/europe_clusters/all_nflg_cluster.csv')
+
+#################################### END #######################################
+################################################################################
