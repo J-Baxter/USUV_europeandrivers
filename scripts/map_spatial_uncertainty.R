454d6c6619ea226ea563ee5e614010218f2c832f
James <james.baxter@ed.ac.uk>
2025-07-07 17:51:24 +0800

    init spatial uncertainty mapping (use heterogenous/homogenous prior distributions for continuous phylogeography)

---
+++ scripts/map_spatial_uncertainty.R
@@ -0,0 +1,295 @@
+################################################################################
+## Script Name:        Tip Location Sampling Uncertainty
+## Purpose:            Edit Beauti XMLs to incorporate location uncertainty 
+## Author:             James Baxter
+## Date Created:       2025-07-07
+## To-do: 1) VectorNet with aggregated mosquito probabilities
+##        2) connection between KML filenames and sequences - /
+################################################################################
+
+############################### SYSTEM OPTIONS #################################
+options(
+  scipen = 6,     # Avoid scientific notation
+  digits = 7      # Set precision for numerical display
+)
+memory.limit(30000000)
+
+############################### DEPENDENCIES ###################################
+# Load required libraries
+library(tidyverse)
+library(magrittr)
+library(terra)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rnaturalearthhires)
+library(giscoR)
+
+# Function adpated from https://github.com/sdellicour/h5n1_mekong
+WriteNUTS3KML <- function(x, prefix = NULL){
+  seq_id <- x$tipnames
+  sampling_probability = x$sampling_probability
+  coords <- x$coords[[1]]
+  
+  sink(file=paste(prefix, seq_id, ".kml", sep=""))
+  cat("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+  cat("\n")
+  cat("<kml xmlns=\"http://earth.google.com/kml/2.2\">")
+  cat("\n")
+  cat(paste("\t<polygon id=\"", paste(seq_id,sep="_"), 
+            "\" samplingProbability=\"", 1, "\">", sep=""))
+  cat("\n")
+  cat("\t\t<coordinates>")
+  cat("\n")
+  
+  for (j in 1:dim(coords)[1]) {
+    cat(paste("\t\t\t", coords[j, 2], ",", coords[j, 1], ",0", sep=""))
+    cat("\n")
+  }
+  cat("\t\t</coordinates>")
+  cat("\n")
+  cat("\t</polygon>")
+  cat("\n")
+  cat("</kml>")
+  cat("\n")
+  sink(NULL)
+  closeAllConnections()
+}
+
+
+# Function adpated from https://github.com/sdellicour/h5n1_mekong
+WriteHeteroKML <- function(x, prefix = NULL){
+
+}
+
+
+# Function from https://github.com/sdellicour/h5n1_mekong
+UpdateBEAUti <- function(xml_filepath){
+  xml = scan(file="BEAST_template.xml", what="", sep="\n", quiet=T)
+  directory = "H5N1_polygons"
+  sink(file="H5N1_clade1.xml")
+  for (i in 1:length(xml)) {
+    cat(xml[i]); cat("\n")
+    if (xml[i]=="\t</continuousDiffusionStatistic>") {
+      cat("\n")
+      for (j in 1:length(sequenceIDs)) {
+        cat(paste("\t<leafTraitParameter id=\"",sequenceIDs[j],".trait\" taxon=\"",names[j],"\">",sep="")); cat("\n")
+        cat(paste("\t\t<treeModel idref=\"treeModel\"/>",sep="")); cat("\n")
+        cat(paste("\t\t<parameter idref=\"leaf.location\"/>",sep="")); cat("\n")
+        cat(paste("\t</leafTraitParameter>",sep="")); cat("\n")
+      }
+      cat("\n")
+      for (j in 1:length(sequenceIDs)) {
+        cat(paste("\t<flatGeoSpatialPrior id=\"",sequenceIDs[j],"_polygons\" taxon=\"",names[j],"\" kmlFileName=\"",directory,"/",sequenceIDs[j],".kml\" inside=\"true\" union=\"true\" cache=\"true\">",sep="")); cat("\n")
+        cat(paste("\t\t<data>",sep="")); cat("\n")
+        cat(paste("\t\t\t<parameter idref=\"",sequenceIDs[j],".trait\"/>",sep="")); cat("\n")
+        cat(paste("\t\t</data>",sep="")); cat("\n")
+        cat(paste("\t</flatGeoSpatialPrior>",sep="")); cat("\n")
+      }
+      cat("\n")		
+    }
+    if (xml[i]=="\t\t</precisionGibbsOperator>") {
+      cat("\n")
+      for (j in 1:length(sequenceIDs)) {
+        cat(paste("\t\t<uniformGeoSpatialOperator weight=\"0.01\">",sep="")); cat("\n")
+        cat(paste("\t\t\t<parameter idref=\"",sequenceIDs[j],".trait\"/>",sep="")); cat("\n")
+        cat(paste("\t\t\t<flatGeoSpatialPrior idref=\"",sequenceIDs[j],"_polygons\"/>",sep="")); cat("\n")
+        cat(paste("\t\t</uniformGeoSpatialOperator>",sep="")); cat("\n")
+      }
+      cat("\n")
+    }
+    if (xml[i]=="\t\t\t\t<multivariateWishartPrior idref=\"location.precisionPrior\"/>") {
+      cat("\n")
+      cat("\t\t\t\t<geoDistributionCollection id=\"allGeoDistributions\">"); cat("\n")
+      for (j in 1:length(sequenceIDs)) {
+        cat(paste("\t\t\t\t<flatGeoSpatialPrior idref=\"",sequenceIDs[j],"_polygons\"/>",sep="")) 
+        cat("\n")
+      }
+      cat("\t\t\t\t</geoDistributionCollection>"); cat("\n")
+      cat("\n")
+    }					
+    if (xml[i]=="\t\t\t<multivariateTraitLikelihood idref=\"location.traitLikelihood\"/>") {
+      if (xml[i-3]=="\t\t\t<strictClockBranchRates idref=\"branchRates\"/>") {
+        cat("\n")
+        for (j in 1:length(sequenceIDs)) {
+          cat(paste("\t\t\t<leafTraitParameter idref=\"",sequenceIDs[j],".trait\"/>",sep="")); cat("\n")
+        }
+        cat("\n")
+      }
+    }	
+  }
+  sink(NULL)
+}
+
+
+################################### DATA #######################################
+# Read and inspect data
+vector_net <- read_sf('./spatial_data/VectornetMAPforMOODjan21.shp', crs = st_crs(nuts0)) %>%
+  st_make_valid() %>%
+  st_transform(st_crs(vector_net))
+
+metadata <- read_csv('./data/USUV_metadata_all_2025Jun24.csv')
+
+nuts0 <- gisco_get_nuts(
+  year = "2021",
+  epsg = "4326", #WGS84 projection
+  resolution = "03", #1:10million
+  nuts_level = "0") 
+
+nuts1 <- gisco_get_nuts(
+  year = "2021",
+  epsg = "4326", #WGS84 projection
+  resolution = "03", #1:10million
+  nuts_level = "1")
+
+nuts2 <- gisco_get_nuts(
+  year = "2021",
+  epsg = "4326", #WGS84 projection
+  resolution = "03", #1:10million
+  nuts_level = "2") 
+
+nuts_all <- bind_rows(nuts0,
+                      nuts1,
+                      nuts2)
+
+################################### MAIN #######################################
+# Main analysis or transformation steps
+
+
+# Pre-format vectornet polygons
+vect_id <- as_tibble(vector_net[-1083,]) %>%
+  rowid_to_column() %>%
+  st_as_sf()
+
+
+# NUTS3 = homogeneous sampling within NUTS3
+metadata %>% 
+  filter(is_europe == '1') %>%
+  filter(location_precision == 'nuts3') %>%
+  dplyr::select(tipnames, eurostat_polygon, 'nuts3_name') %>%
+  
+  # Join polygons and convert to matrix form. Selecting best resolution by default
+  left_join(vect_id, by = join_by('eurostat_polygon' == 'rowid')) %>%
+  mutate(coords = list(unlist(geometry, recursive = FALSE)[[1]][[1]]),
+         sampling_probability = 1) %>%
+  dplyr::select(1,5,6) %>%
+  
+  # splt dataframe by sequence
+  rowid_to_column(var = 'id') %>%
+  group_split(id) %>%
+  
+  # write KML file for each sequence
+  lapply(., WriteNUTS3KML, prefix = './2025Jun24/kmls/')
+
+
+# NUTS0, NUTS1, and NUTS2 (sampling determined by culex abundance )
+testnuts1 <- metadata %>% 
+  filter(is_europe == '1') %>%
+  filter(location_precision %in% c('nuts0', 'nuts1', 'nuts2')) %>%
+  dplyr::select(tipnames, ends_with('_id')) %>%
+  pivot_longer(cols = -1, names_to = 'nuts_level', values_to = 'nuts_id') %>%
+  drop_na() %>%
+  mutate(nuts_level = gsub('_id', '', nuts_level)) %>%
+  
+  # Join current polygon
+  left_join(nuts_all %>% dplyr::select(NUTS_ID, geometry),
+            by = join_by('nuts_id' == 'NUTS_ID')) %>%
+  
+  # list all vectornet polygons within current polygon
+  mutate(within_geoms = map(geometry, ~ {
+    which(st_overlaps(vect_id$geometry, .x, sparse = FALSE)[, 1] | st_within(vect_id$geometry, .x, sparse = FALSE)[, 1] )
+  }))
+
+  # join vectornet polygons with agregated mosquito probability. 
+  ##HERE## 
+  # convert to matrix form
+  left_join(vect_id, by = join_by('eurostat_polygon' == 'rowid')) %>%
+  mutate(coords = list(unlist(geometry, recursive = FALSE)[[1]][[1]]),
+         sampling_probability = 1) %>%
+  dplyr::select(1,5,6) %>% 
+  
+  # splt dataframe by sequence
+  rowid_to_column(var = 'id') %>%
+  group_split(id) %>%
+  
+  # write KML file for each sequence
+  lapply(., WriteNUTS3KML, prefix = './2025Jun24/kmls/')
+  
+  
+  vect_id[c(30, 31, 32, 33, 54, 94, 95, 96, 102, 103, 104, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 148, 396, 397, 404, 755, 884, 886, 887, 888, 895, 937, 938, 939, 940, 942),] %>%
+    ggplot()+ 
+    geom_sf(data = nuts0 %>% filter(CNTR_CODE == 'DE')) +
+    geom_sf(colour = 'white') + 
+
+    #geom_sf(colour = 'white', data = nuts0, linewidth = 0.5, alpha = 0.01) + 
+    coord_sf(ylim = c(40,55), xlim = c(0, 30), expand = FALSE) 
+
+map_metadata <- metadata %>%
+  
+  # format geo
+  mutate(geocode_coords =  gsub('c\\(|\\)', '', geocode_coords)) %>%
+  separate(geocode_coords, into = c("lat", "lon"), sep = ", ", convert = TRUE) %>%
+  st_as_sf(coords = c('lat','lon'), crs = st_crs(vector_net), sf_column_name = 'coords') %>%
+  st_join(.,
+          vect_id, 
+          join = st_within, 
+          # left = FALSE,
+          # largest = TRUE
+  ) %>% 
+  drop_na(nuts2_id) %>%
+  
+  # format 
+  #left_join(lineage_tbl, by = join_by('tipnames' == 'label')) %>%
+  #drop_na(`GRI Lineage Level 1`) %>%
+  count( rowid) %>%
+  #rename(lineage = `GRI Lineage Level 1`) %>%
+  st_drop_geometry()
+
+
+map_data <- expand_grid(rowid = vect_id$rowid,
+                        n = NA_integer_) %>%
+  rows_patch(map_metadata,
+             unmatched = 'ignore',
+             by = c('rowid')) %>%
+  left_join(vect_id) #%>%
+  #filter(grepl('^A', lineage)) 
+# filter(CNTR_CODE != 'TR') 
+
+
+map_data %>%
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = n), colour = 'white') + 
+  #geom_sf(colour = 'white', data = nuts0, linewidth = 0.5, alpha = 0.01) + 
+  coord_sf(ylim = c(34,72), xlim = c(-11, 34), expand = FALSE) +
+  scale_fill_distiller(palette = 'RdPu', 
+                       #transform = 'log10', 
+                       direction = -1,  
+                       na.value="lightgrey") +
+  #facet_wrap(~lineage) +
+  theme_void() + 
+  theme(legend.position = 'none')
+
+
+# Exact = Exact
+
+
+
+
+
+
+WriteNUTS3KML(test_nuts3[[1]])
+
+
+
+
+
+################################### OUTPUT #####################################
+
+# Update Beauti 
+
+
+
+
+#################################### END #######################################
+################################################################################
