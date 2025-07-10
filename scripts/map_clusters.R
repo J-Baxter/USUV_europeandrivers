fbf2ef270d874a0da08559f90c499b0f26984e8f
James <james.baxter@ed.ac.uk>
2025-07-10 15:00:07 +0800

    containerise cluster allocation pipe to apply to both partial and full genomes; extend script to include allocation of clusters to partial genomes; export single joint csv containing all cluster (but not lineage) assignments

--- scripts/map_clusters.R
+++ scripts/map_clusters.R
@@ -25,64 +25,81 @@ library(ggnewscale)
 library(ggtreeExtra)
 library(phangorn)
 
+
+MapClusters <- function(new_tree, cluster_tbl){
+  
+  tree_tbl <- as_tibble(new_tree)
+  
+  mrca_nodes <- new_tree %>%
+    
+    # ML tree to tree-data object and add cluster/lineage assignments
+    as_tibble() %>%
+    left_join(cluster_tbl %>% dplyr::select(label, dist_30)) %>%
+    
+    # Exclude problematic cluster I
+    filter(dist_30 != 'I') %>%
+    
+    # Exclude all NAs and split by cluster
+    drop_na(dist_30) %>%
+    group_split(dist_30) %>%
+    setNames(LETTERS[1:length(.)]) %>%
+    
+    # Find common ancestor nodes of clusters/lineages 
+    lapply(., function(x) MRCA(new_tree, .node1 = x$label)) %>%
+    enframe(name = 'cluster', value = 'root') %>%
+    mutate(root = as.integer(root))
+  
+  
+  # Find all descendants of common ancestor nodes
+  all_seq_clusters <- mrca_nodes %>%  
+    pull(root) %>%
+    lapply(. , function(x) tree_tbl %>% 
+             filter(node %in% offspring(new_tree, x, type = 'tips')) %>%
+             pull(label)) %>%
+    lapply(., as_tibble) %>%
+    setNames(LETTERS[1:length(.)]) %>%
+    bind_rows(, .id = 'cluster') %>%
+    rename(label = value)
+  
+  return(all_seq_clusters)
+}
+
+
 ################################### DATA #######################################
 # Read and inspect data
+
+# Previously inferred data
+# subsampled beast tree
 nflg_hipstr <- read.beast('./2025Jun24/global_analysis/USUV_2025Jun24_NFLG_SRD06_HMC_constant_hipstr.tree')
-
-nflg_ml <- read.newick('./2025Jun24/global_analysis/phylogenetic_placement/USUV_2025Jun24_alldata_aligned_formatted_noFLI_NFLG_subsampled.fasta.contree') %>% 
-  phytools::midpoint_root()
-
 subsample_clusterings <- read_csv('./2025Jun24/europe_clusters/subsample_clusterings.csv')
-
 subsample_nomenclature <- read_delim('./2025Jun24/nomenclature/subsample/USUV_2025Jun24_labels.tsv') %>%
   rename(label = sample)
 
+# New data
+# all NFLG sequences (ML)
+nflg_ml <- read.newick('./2025Jun24/europe_clusters/nflg_phyloplacement/USUV_2025Jun24_alldata_aligned_formatted_noFLI_NFLG.fasta.contree') %>% 
+  phytools::midpoint_root()
+
+# subsampled NFLG and all concat/partial sequences (ML)
+partial_ml <- read.newick('./2025Jun24/europe_clusters/partial_seq_phyloplacement/USUV_2025Jun24_alldata_aligned_formatted_subsampled_incpartial.fasta.treefile') %>% 
+  phytools::midpoint_root()
+
+
 ################################### MAIN #######################################
 # Main analysis or transformation steps
-mrca_nodes <- nflg_ml %>%
+all_nflg_clusters <- MapClusters(nflg_ml, subsample_clusterings)
+all_partial_clusters <- MapClusters(partial_ml, subsample_clusterings)
 
-  # ML tree to tree-data object and add cluster/lineage assignments
-  as_tibble() %>%
-  left_join(subsample_clusterings %>% dplyr::select(label, dist_30)) %>%
-  
-  # Exclude problematic cluster I
-  filter(dist_30 != 'I') %>%
-
-  # Exclude all NAs and split by cluster
-  drop_na(dist_30) %>%
-  group_split(dist_30) %>%
-  setNames(LETTERS[1:length(.)]) %>%
-  
-  # Find common ancestor nodes of clusters/lineages 
-  lapply(., function(x) MRCA(nflg_ml, .node1 = x$label)) %>%
-  enframe(name = 'cluster', value = 'root') %>%
-  mutate(root = as.integer(root))
-
+all_clusters <-  bind_rows(all_nflg_clusters,
+                           all_partial_clusters) %>%
+  distinct(label)
 
 # visual checks
-#nflg_ml %>%
-  #left_join(mrca_nodes %>% rename(node = root)) %>%
-  #ggtree() + 
-  #geom_nodepoint(aes(colour = cluster))
-
-
-# Find all descendants of common ancestor nodes
-all_seq_clusters <- mrca_nodes %>%  
-  pull(root) %>%
-  lapply(. , function(x) ml_tbl %>% 
-                     filter(node %in% offspring(nflg_ml, x, type = 'tips')) %>%
-                     pull(label)) %>%
-  lapply(., as_tibble) %>%
-  setNames(LETTERS[1:length(.)]) %>%
-  bind_rows(, .id = 'cluster') %>%
-  rename(label = value)
-
-# visual checks
-nflg_ml %>%
-  left_join(all_seq_clusters) %>% 
+partial_ml %>%
+  left_join(all_partial_clusters) %>% 
   ggtree(.) + 
   geom_tippoint(aes(colour = cluster))
-  
+
 
 # Cluster X lineage comparison
 subsample_clusterings %>% dplyr::select(label, dist_30) %>%
@@ -104,18 +121,17 @@ lineage_colours <- c('A' = '#a4243b',
 nflg_hipstr %>%
   left_join(all_seq_clusters) %>% 
   ggtree(mrsd = '2024-10-12') +
-
-geom_cladelab(data = level_0_tbl %>% rename(lineage_label = lineage) %>% 
-                mutate(level_0 = gsub('\\..*', '', lineage_label)),
-              mapping = aes(node = root_node, 
-                            label = lineage_label, 
-                            colour = level_0),
-              offset = -32,
-              offset.text = -17,
-              fontsize = 3, 
-              #textcolour = NA,
-              #size = 0.01,
-              align = T) + 
+  geom_cladelab(data = level_0_tbl %>% rename(lineage_label = lineage) %>% 
+                  mutate(level_0 = gsub('\\..*', '', lineage_label)),
+                mapping = aes(node = root_node, 
+                              label = lineage_label, 
+                              colour = level_0),
+                offset = -32,
+                offset.text = -17,
+                fontsize = 3, 
+                #textcolour = NA,
+                #size = 0.01,
+                align = T) + 
   scale_colour_manual(values = lineage_colours, guide = 'none')+
   new_scale_colour() +
   
@@ -136,13 +152,11 @@ geom_cladelab(data = level_0_tbl %>% rename(lineage_label = lineage) %>%
   scale_colour_manual(values = lineage_colours, guide = 'none') +
   new_scale_colour() + 
   geom_tippoint(aes(colour = cluster))
-  
+
 
 ################################### OUTPUT #####################################
 # Save output files, plots, or results
-
-# all nflg clusters
-write_csv(all_seq_clusters, './2025Jun24/europe_clusters/all_nflg_cluster.csv')
-
+# all clusters
+write_csv(all_clusters, './2025Jun24/europe_clusters/all_clusters.csv')
 #################################### END #######################################
 ################################################################################
