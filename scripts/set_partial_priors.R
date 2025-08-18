4190477b48830a911cd7a0378c0dedef75e1c134
James <james.baxter@ed.ac.uk>
2025-08-18 14:27:19 +0100

    init set partial priors

---
+++ scripts/set_partial_priors.R
@@ -0,0 +1,31 @@
+################################################################################
+## Script Name:        <INSERT_SCRIPT_NAME_HERE>
+## Purpose:            <BRIEFLY_DESCRIBE_SCRIPT_PURPOSE>
+## Author:             James Baxter
+## Date Created:       2025-08-18
+################################################################################
+
+############################### SYSTEM OPTIONS #################################
+options(
+  scipen = 6,     # Avoid scientific notation
+  digits = 7      # Set precision for numerical display
+)
+memory.limit(30000000)
+
+############################### DEPENDENCIES ###################################
+# Load required libraries
+library(tidyverse)
+library(magrittr)
+
+
+################################### DATA #######################################
+# Read and inspect data
+
+################################### MAIN #######################################
+# Main analysis or transformation steps
+
+################################### OUTPUT #####################################
+# Save output files, plots, or results
+
+#################################### END #######################################
+################################################################################
