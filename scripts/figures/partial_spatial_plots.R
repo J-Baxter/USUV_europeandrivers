9b6710b6911efa8e87bbf16ca4631192dd91254c
James <james.baxter@ed.ac.uk>
2025-11-13 19:24:38 +0000

    init basic line plots for spatial statistics

--- scripts/figures/partial_spatial_plots.R
+++ scripts/figures/partial_spatial_plots.R
@@ -28,8 +28,60 @@ library(treeio)
 library(scales)
 
 
+ReadSpatialStatistics <- function(filepath){
+  out <-  read_delim(filepath,
+                     delim = '\t', 
+                     name_repair = function(nm) {
+                       nm[2:length(nm)] <- paste0(seq_len(length(nm) - 1))
+                       nm}) %>%
+    pivot_longer(-1, names_to = 'iter', values_to = 'draw') 
+  
+  return(out)
+  
+}
+
 ################################### DATA #######################################
 # Read and inspect data
+dirs <- list.dirs("./2025Jun24/europe_clusters", recursive = FALSE)
+dirs <- dirs[grepl("_III|_V|_VI", basename(dirs))]
+
+wavefront_files <- sapply(dirs, list.files, pattern = 'wavefront_distances.txt',
+                          full.names = TRUE, 
+                          simplify = F) %>%
+  Filter(length,.) %>% 
+  flatten_chr()
+
+diffusioncoef_files <- sapply(dirs, list.files, pattern = 'tree_weighted_diffusion_coefficient.txt',
+                          full.names = TRUE, 
+                          simplify = F) %>%
+  Filter(length,.) %>% 
+  flatten_chr()
+
+wavefront_tbl <- lapply(wavefront_files, ReadSpatialStatistics) %>%
+  setNames(wavefront_files) %>%
+  bind_rows(.id = 'filename')  %>%
+  separate_wider_delim(filename, delim = '/', names = c('file0', 'file1', 'file2', 'clade', 'metric')) %>%
+  dplyr::select(-starts_with('file')) %>%
+  mutate(metric = gsub('USUV_|s.txt', '', metric))
+
+diffusioncoef_tbl <- lapply(diffusioncoef_files, ReadSpatialStatistics) %>%
+  setNames(diffusioncoef_files) %>%
+  bind_rows(.id = 'filename')  %>%
+  separate_wider_delim(filename, delim = '/', names = c('file0', 'file1', 'file2', 'clade', 'metric')) %>%
+  dplyr::select(-starts_with('file')) %>%
+  mutate(metric = gsub('USUV_tree_|\\.txt', '', metric))
+  
+wavefront_tbl %>%
+  ggplot() + 
+  geom_path(aes(x = time, y = draw, colour = clade, group = iter)) + 
+  facet_grid(rows = vars(metric), cols = vars(clade))
+
+
+diffusioncoef_tbl %>%
+  ggplot() + 
+  geom_path(aes(x = time, y = draw, colour = clade, group = iter)) + 
+  facet_grid(rows = vars(clade), scales = 'free_y')
+
 
 ################################### MAIN #######################################
 # Main analysis or transformation steps
