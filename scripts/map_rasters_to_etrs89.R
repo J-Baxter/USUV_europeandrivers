df2381a7680eac183ae5171c6af0d65b30ac1911
James <james.baxter@ed.ac.uk>
2025-11-21 17:53:06 +0000

    update dependencies; include fao, population and elevation

--- scripts/map_rasters_to_etrs89.R
+++ scripts/map_rasters_to_etrs89.R
@@ -2,8 +2,9 @@
 ## Script Name:        Format Rasters
 ## Purpose:            Extract and format environmental and ecological raster 
 ##                     layers for analysis in Seraphim (Dellicour et al. 2016). 
-##                     For consistency, we grid all rasters using the ETRS89 10km
-##                     grid.
+##                     For consistency, we grid all rasters to the 10 km x 10 km 
+##                     ETRS89 grid, using the ETRS89-extended / LAEA Europe 
+##                     (EPSG:3035) projection.
 ## Author:             James Baxter
 ## Date Created:       2025-09-19
 ################################################################################
@@ -24,6 +25,7 @@ library(tidyterra)
 library(sf)
 library(stars)
 library(exactextractr)
+library(giscoR)
 
 FilterCorine <- function(raster, selected_layers){
   out <- raster %in% selected_layers
@@ -55,9 +57,10 @@ SaveLayer <- function(sf_dataframe, raster_name){
   write_stars(raster, paste0(filename, '.tif'))
   
   # as shapefile
-  st_write(sf_dataframe, paste0(filename, '.shp'))
+  sf::write_sf(sf_dataframe, 
+               paste0(filename, '.shp'))
   
-  cat(paste0('Saved ', raster_name, ' successfully.'))
+  cat(paste0('Saved ', raster_name, ' successfully.\n'))
 }
 
 
@@ -83,8 +86,16 @@ fao_list <- list.files('./2025Jun24/raster_data/fao',
                        full.names = T)
 fao_rast <- lapply(fao_list, rast)
 
+# EU-DEM data
+elevation_original <- rast('./2025Jun24/raster_data/EU_DEM_mosaic_1000K/eudem_dem_3035_europe.tif')
+
 ################################### MAIN #######################################
 # Main analysis or transformation steps
+# Set bounding box
+bounding_box <- st_bbox(c(xmin = 2500000, xmax = 6700000, 
+                          ymin = 1200000, ymax = 5500000), 
+                        crs = st_crs(ETRS89_10)) %>%
+  st_as_sfc()
 
 # 1. CORINE
 # Only keep CORINE data within Europe mask
@@ -98,18 +109,12 @@ level_two_dict <- corine_legend %>%
   lapply(., function(x) x %>% pull(LABEL3)) %>%
   as.list()
 
-#corine_grouped <- list()
-#corine_grouped[[1]] <- FilterCorine(corine_all_masked, level_two_dict[[1]])
 corine_grouped <- lapply(level_two_dict, FilterCorine, raster = corine_all_masked) %>%
   setNames(names(level_two_dict))
 
-#corine_agregated[[1]] <- AggregateCorine(corine_grouped[[1]], ETRS89_10)
-corine_agregated <- lapply(corine_grouped, AggregateCorine, grid_vector = ETRS89_10) %>%
+corine_agregated <- lapply(corine_grouped, AggregateLayer, grid_vector = ETRS89_10) %>%
+  lapply(., function(x) x[st_intersects(x, bounding_box, sparse = FALSE),]) %>%
   setNames(names(level_two_dict))
-
-mapply(SaveLayer, 
-       corine_agregated, 
-       names(level_two_dict))
 
 
 # 2. EBBA
@@ -120,11 +125,8 @@ birds <- ebba_csv %>%
 ebba_gridded <- ebba_csv %>%
   rename(value = 'Probability of occurrence') %>%
   split(~ birdlife_scientific_name) %>%
-  map(~ ETRS89_10 %>% left_join(.x)) 
-
-mapply(SaveLayer, 
-       ebba_gridded, 
-       names(ebba_gridded))
+  map(~ ETRS89_10 %>% left_join(.x)) %>%
+  lapply(., function(x) x[st_intersects(x, bounding_box, sparse = FALSE),])
 
 
 # 3. FAO 
@@ -134,24 +136,69 @@ fao_reprojected <- fao_rast %>%
     x <- project(x, 'EPSG:3035')
     return(x)}) 
 
-fao_gridded <- lapply(fao_reprojected, AggregateLayer, grid_vector = ETRS89_10)
+fao_gridded <- lapply(fao_reprojected, AggregateLayer, grid_vector = ETRS89_10)%>%
+  lapply(., function(x) x[st_intersects(x, bounding_box, sparse = FALSE),])
 
 fao_names <- paste0('GLW4-2020_', c('chicken', 'cattle', 'goat', 'swine', 'sheep'))
 
+
+# 4. Elevation
+elevation_gridded <- elevation_original %>%
+  AggregateLayer(grid_vector = ETRS89_10) %>%
+  .[st_intersects(., bounding_box, sparse = FALSE),] 
+
+
+# 5. Eurostat Human Population
+log10_population <- gisco_get_grid(
+  resolution = "10",
+  spatialtype = c("REGION")) %>%
+  .[st_intersects(., bounding_box, sparse = FALSE),] %>%
+  dplyr::select(TOT_P_2018, geom) %>%
+  rename(geometry = geom) %>%
+  mutate(value = log10(TOT_P_2018)) %>%
+  dplyr::select(-TOT_P_2018)
+
+
+# 6. ERA5 (Monthly)
+
+
+
+# 7. Bioclimatic
+
+
+################################### OUTPUT #####################################
+# Save output files, plots, or results
+# 1. CORINE
+mapply(SaveLayer, 
+       corine_agregated, 
+       names(level_two_dict))
+
+# 2. EBBA
+mapply(SaveLayer, 
+       ebba_gridded, 
+       names(ebba_gridded))
+
+# 3. FAO
 mapply(SaveLayer, 
        fao_gridded, 
        fao_names)
 
+# 4. 
+SaveLayer(elevation_gridded, 'elevation')
 
-# 4. Elevation
+# 5. Eurostat Human Population
+SaveLayer(log10_population, 'log10_humanpopulation')
 
-# 5. Human Population
 
-# 6. ERA5 (Monthly)
-
-# 7. Bioclimatic
-################################### OUTPUT #####################################
-# Save output files, plots, or results
+# need to add bounding box to all
+elevation_gridded %>%
+ggplot() +
+  geom_sf(aes(fill = value), colour = '#ff000000') + 
+  #coord_sf(datum = sf::st_crs(ETRS89_10),
+  #ylim = c(1200000, 5500000),
+  #xlim = c(2500000, 6700000), 
+  #expand = FALSE) 
+  scale_fill_continuous(trans = 'log10')
 
 #################################### END #######################################
 ################################################################################
